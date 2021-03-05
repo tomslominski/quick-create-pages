91fa91f78f869b2a37a851541f51b08df4370325
Tom Slominski <tom@slomin.ski>
2021-03-05 23:08:21 +0000

    Tidy up page saving, add better sanitization

--- app/SubmissionHandler.php
+++ app/SubmissionHandler.php
@@ -43,8 +43,8 @@ class SubmissionHandler
 	 * Save the pages.
 	 */
 	public function process_submission() {
-		$post_type = $_POST['qcp']['post_type'] ?? 'page';
-		$pages = $_POST['qcp']['pages'] ?? [];
+		$post_type = $_POST['qcp']['post_type'] ?? null;
+		$pages = $_POST['qcp']['pages'] ?? null;
 		$default = [
 			[
 				'name' => '',
@@ -52,28 +52,43 @@ class SubmissionHandler
 			],
 		];
 
+		// Check if nonce is valid
 		if( !isset( $_POST['qcp']['nonce'] ) || !wp_verify_nonce( $_POST['qcp']['nonce'], 'qcp_create_pages' ) ) {
 			$this->errors[] = __( 'An error occurred. Please try creating the pages again.', 'quick-create-pages' );
 		}
 
-		if( $pages && $pages !== $default ) {
+		// Check if post type is valid
+		if( !isset( $post_type ) || !in_array( $post_type, get_post_types( [
+			'public' => true,
+		] ) ) ) {
+			$this->errors[] = __( 'An incorrect post type was set. Please try creating the pages again.', 'quick-create-pages' );
+			$post_type = 'page';
+		}
+
+		// Check if posts were provided
+		if( !isset( $pages ) || $pages === $default ) {
+			$this->errors[] = __( 'No pages were added. Please try creating the pages again.', 'quick-create-pages' );
+		}
+
+		if( !$this->errors ) {
+			$this->parse_pages( $pages );
 			$this->save_pages( $pages, $post_type );
+		}
 
-			if( $this->errors ) {
-				// Hand the data back to the Vue script in the case of an error
-				if( isset( $_POST['qcp']['pages'] ) ) {
-					add_filter( 'qcp/js_config', function( array $config ) {
-						$config['pages'] = $_POST['qcp']['pages'];
-						$config['selectedPostType'] = $_POST['qcp']['post_type'] ?? 'page';
+		if( $this->errors ) {
+			// Hand the data back to the Vue script in the case of an error
+			if( isset( $_POST['qcp']['pages'] ) ) {
+				add_filter( 'qcp/js_config', function( array $config ) use ( $pages, $post_type ) {
+					$config['pages'] = $pages;
+					$config['selectedPostType'] = $post_type;
 
-						$this->parse_pages( $config['pages'] );
+					$this->parse_pages( $config['pages'] );
 
-						return $config;
-					} );
-				}
-			} else {
-				$this->messages[] = __( 'Pages created successfully.', 'quick-create-pages' );
+					return $config;
+				} );
 			}
+		} else {
+			$this->messages[] = __( 'Pages created successfully.', 'quick-create-pages' );
 		}
 	}
 
@@ -89,6 +104,9 @@ class SubmissionHandler
 				'slug' => '',
 				'children' => [],
 			], $page );
+
+			$page['name'] = sanitize_text_field( $page['name'] );
+			$page['slug'] = sanitize_title( $page['slug'] );
 
 			if( $page['children'] ) {
 				$this->parse_pages( $page['children'] );
